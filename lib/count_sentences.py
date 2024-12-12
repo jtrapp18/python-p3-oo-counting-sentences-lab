84aeb5548c3159d6d0958f56b8675beb46703b88
+ <jacqueline.trapp18@gmail.com>
2024-12-12 00:57:18 -0500

    lab complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+  
+  def __init__(self, value="123"):
+    self.value = value
+
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if isinstance(value, str):
+      self._value = value
+    else:
+      print("The value must be a string.")
+    
+  def is_sentence(self):
+    return True if self._value[-1] == "." else False
+  
+  def is_question(self):
+    return True if self._value[-1] == "?" else False
+  
+  def is_exclamation(self):
+    return True if self._value[-1] == "!" else False
+  
+  def count_sentences(self):
+    return len(re.findall("[^.?!][.?!]", self._value))
+
+  
+
+
+
